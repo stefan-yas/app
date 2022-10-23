d0448b9dadde6ef69403d11db208850360481556
Stefan Yas <stefan.sy.yas@gmail.com>
2022-10-23 18:10:56 +0200

    fix(cypress): disabled video recording in ci/cd

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3001'
+    baseUrl: 'http://localhost:3001',
+    "video": false
   }
 })
